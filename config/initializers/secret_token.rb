9c0324cfd0d0039698d5f178ba936c819bbe9d7b
Tom Brennan <tommybrennan@gmail.com>
2014-09-05 16:35:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HorseFarm::Application.config.secret_key_base = 'a6d569dabc0eb1a29f970972b5d988efdf42f02378b2819c07169909dc53df420d9865c06769ff2c72c33ec2f0cdc847d6b68f14ebf9a80ba66adff3cd219869'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HorseFarm::Application.config.session_store :cookie_store, key: '_horse_farm_session'
